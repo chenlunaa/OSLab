30d212fc49d8f257f710d92e8b1f414f60a9efb5
chenlun <2146529322@qq.com>
2026-09-12 11:07:05 +0800

    增加了sleep

---
+++ user/sleep.c
@@ -0,0 +1,17 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+// 调用内核的sleep
+int
+main(int argc, char *argv[])
+{
+  if(argc != 2){
+    printf("Sleep needs one argument!\n");
+    exit(-1);
+  }
+
+  int ticks = atoi(argv[1]);
+  sleep(ticks);
+  printf("Sleep %d\n", ticks);
+  exit(0);
+}
